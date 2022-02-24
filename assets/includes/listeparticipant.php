f88ee4492c65f9178aeee04ea306bf192ee369d3
k-vin <vinz.sosaity@gmail.com>
2022-02-24 15:55:45 +0100

    first commit

---
+++ assets/includes/listeparticipant.php
@@ -0,0 +1,33 @@
+<div class="participant">
+    <div class="liste">
+        <div class="nom">
+            <h2>NOM</h2>
+            <p>Carpenter</p>
+            <p>Scoot</p>
+            <p>Valentine</p>
+            <p>Redfield</p>
+            <p>Linch</p>
+            <p>Willis</p>
+            <p>Warren</p>
+            <p>Dewab</p>
+        </div>
+
+        <div class="prenom">
+            <h2>Prenom</h2>
+            <p>John</p>
+            <p>Travis</p>
+            <p>Jill</p>
+            <p>Chris</p>
+            <p>David</p>
+            <p>Bruce</p>
+            <p>Ed</p>
+            <p>Kévin</p>
+        </div>
+
+        <div class="date">
+            <h2>date d'inscription</h2>
+            <p>25/01/2022</p>
+        </div>
+
+    </div>
+</div>
