c2b2980db993090e42cd28c1dd9ab9ad39e9331b
TeddyH08 <teddy.hemonet@gmail.com>
2022-02-24 14:36:19 +0100

    first commit

---
+++ classementgeneral.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tournoi Simplon</title>
+    <link rel="stylesheet" href="assets/styles/style.css">
+</head>
+
+<body>
+    <?php include("assets/inculdes/navbar.php") ?>
+    <?php include("assets/inculdes/classement.php") ?>
+    <?php include("assets/inculdes/footer.php") ?>
+</body>
+</html>
