0b61e0611f484a1dc91070cbcb0f42f54f55c221
mikaelhen <mikaelhen28@gmail.com>
2022-02-24 23:27:41 +0100

    23h27

--- assets/includes/navbar.php
+++ assets/includes/navbar.php
@@ -1,6 +1,11 @@
 <div class="topnav">
-  <a class="active" href="#Accueil">Accueil</a>
-  <a href="#Jeux">Jeux</a>
-  <a href="#formulaire">formulaire</a>
-  <a href="#Classement">Classement</a>
+     <div class="logo">
+           <img src="./assets/img/logotournoi.png">
+    </div>
+    <div class="menu">
+        <a class="active" href="#Accueil">Accueil</a>
+        <a href="#Jeux">Jeux</a>
+        <a href="#formulaire">formulaire</a>
+        <a href="#Classement">Classement</a>
+    </div>
 </div> 
